986adac240435af858b521f8427dd4a49ca52a32
santosalisson28-byte <santos.alisson28@escola.pr.gov.br>
2026-09-23 08:34:24 -0300

    atualizei meu codigo

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const botao = document.queryselector("#botaoMensagem");
-const mensagem = documnt.queryselector("#mensagemInterativa");
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir)
+    let curtiu = false;
+    botaoCurtir.addEventListener("click",curtir);
+    function curtir(){
+        const Contador = botaoCurtir.querySelector("span");
+        if(curtium=== false){
+            contador.textContent++;
+            curtiu = true;
+            else{
+                contador.textContent--;
+                curtiu = false;
+            
+        }
+    }
+})
